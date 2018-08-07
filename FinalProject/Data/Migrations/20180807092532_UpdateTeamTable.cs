28b2c98af6f226ff3eb41bf4c9591319d9b822ea
HenBenshimol <Henbs555@gmail.com>
2018-08-07 12:28:04 +0300

    Create Team model

---
+++ FinalProject/Data/Migrations/20180807092532_UpdateTeamTable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace FinalProject.Data.Migrations
+{
+    public partial class UpdateTeamTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Teams",
+                columns: table => new
+                {
+                    TeamID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Description = table.Column<string>(nullable: true),
+                    Lattitude = table.Column<double>(nullable: false),
+                    Longtidute = table.Column<double>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Zoom = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teams", x => x.TeamID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Teams");
+        }
+    }
+}
---
+++ FinalProject/Models/Team.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.Models
+{
+    public class Team
+    {
+        public int TeamID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public double Zoom { get; set; }
+
+        public double Lattitude { get; set; }
+
+        public double Longtidute { get; set; }
+
+        public Team() { }
+
+        public Team(int teamId, string name, string description, double zoom, double lattitude, double longtidute)
+        {
+            this.TeamID = teamId;
+            this.Name = name;
+            this.Description = description;
+            this.Zoom = zoom;
+            this.Lattitude = lattitude;
+            this.Longtidute = longtidute;
+        }
+    }
+}
